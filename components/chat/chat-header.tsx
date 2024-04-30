327f489f05aa51445980e8f3df9a9e4387ba79f9
Rishikesh Ghosh <rishikeshghoshghosh@gmail.com>
2024-04-30 14:12:06 +0530

    Update chat-header.tsx

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -4,7 +4,7 @@ import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 import { SocketIndicator } from "@/components/socket-indicator";
 
-//import { ChatVideoButton } from "./chat-video-button";
+import { ChatVideoButton } from "./chat-video-button";
 
 interface ChatHeaderProps {
   serverId: string;
